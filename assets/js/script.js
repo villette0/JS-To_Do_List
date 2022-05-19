3c55c032ac229fb5ef7b5661ead0e86741dfd2e2
Villette Comfort <villette@live.com>
2022-05-19 00:25:07 -0700

    Added universal variable selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
-// Universal variables first
-
-
+// Universal variables, Selectors first
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
